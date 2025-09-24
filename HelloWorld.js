2257e92350efad894bd32860565afff93da1953f
salma <slmasamytaha@gmail.com>
2025-09-24 12:04:01 +0300

    add class inside it method print hello world

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,7 +1,7 @@
-// class HelloWorld{
-//  printHelloWorld(){
-//     console.log("HelloWorld")
-//  }
-// }
-// let hello= new HelloWorld();
-// hello.printHelloWorld();
+class HelloWorld{
+ printHelloWorld(){
+    console.log("HelloWorld")
+ }
+}
+let hello= new HelloWorld();
+hello.printHelloWorld();
